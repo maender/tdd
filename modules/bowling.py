799ed5c1e40806c80be944cb8f31c6dff27be9e8
maender <selim.dincbudak@gmail.com>
2022-03-17 11:41:21 +0100

    changing new input

--- modules/bowling.py
+++ modules/bowling.py
@@ -7,6 +7,7 @@ STRIKE_OR_SPARE_SCORE = 10
 end_of_unfinished_game = lambda x, idx : len(x) < 10 and idx == len(x) - 1
 is_strike_or_spare = lambda x, idx : idx < 9 and sum(x) == 10
 is_strike = lambda x : x[0] == 10
+next_2_throws = lambda x, idx : x[idx + 1: idx + 3]
 
 def calculate_second_strike_bonus(next_throws : list, idx : int):
     total = 0
@@ -38,7 +39,7 @@ def calculate_spare_bonus(next_throws : list, idx : int):
 
     return total
 
-def calculate_bonus(next_throws : list, idx : int, strike : bool):
+def calculate_bonus(next_throws : list, idx : int, strike : bool, throw_list : list = None):
     total = 0
 
     if strike:
@@ -48,7 +49,35 @@ def calculate_bonus(next_throws : list, idx : int, strike : bool):
 
     return total
 
-def bowling_score(frames : list):
+def format_input(throws : list):
+    out = list()
+
+    throw = 0
+    throw1 = 0
+    throw2 = 0
+    frame = 0
+    for val in throws:
+        if val == 10 and throw == 0 and frame < 9:
+            out.append(STRIKE)
+            frame += 1
+        elif throw == 0:
+            throw1 = val
+            throw += 1
+        elif frame < 9:
+            out.append((throw1, val))
+            throw = 0
+            frame += 1
+        elif throw < 2:
+            throw2 = val
+            throw += 1
+        else:
+            out.append((throw1, throw2, val))
+            throw += 1
+    if throw < 3:
+        out.append((throw1, throw2))
+    return out
+
+def bowling_score(throws : list):
     '''
     In:
     Scores is a list of 10 tuples (x, y)
@@ -61,13 +90,29 @@ def bowling_score(frames : list):
     Point total
     '''
 
+    frames = format_input(throws)
     total = 0
+    totalStrikes = 0
+    frame = 0
+    chance = 0
+    for idx, throw in enumerate(throws):
+        if throw == 10 and chance == 0:
+            totalStrikes += STRIKE_SCORE + calculate_strike_bonus(None, None, True, next_2_throws(throws, idx))
+            frame += 1
+        elif chance == 1:
+            frame += 1
+            chance = 0
+            pass
+        
+            
+
     for idx, frame in enumerate(frames):
         if is_strike_or_spare(frame, idx):
             if end_of_unfinished_game(frames, idx):
                 break
             total += STRIKE_OR_SPARE_SCORE + calculate_bonus(frames, idx, is_strike(frame))
-        else: total += sum(frame)
+        else:
+            total += sum(frame)
 
     return total
 
--- tests/test_bowling.py
+++ tests/test_bowling.py
@@ -9,67 +9,58 @@ Strikes are represented by the (10, 0) tuple except for last frame
 because then you have 3 throws
 '''
 
+# [0,10,10,10,10,10,10,10,10,10,10,10,10]
+
 def test_emptyGame():
-    frames = [(0, 0) for x in range(10)]
+    frames = [0 for x in range(20)]
     assert bowling_score(frames) == 0
 
 def test_gameWith10Pts():
-    frames = [(1, 0) for x in range(10)]
+    frames = [1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0,1, 0]
     assert bowling_score(frames) == 10
 
 def test_gameWithOneSpareAtFisrtFrame():
-    frames = [(1, 0) for x in range(10)]
-    frames[0] = (9, 1)
+    frames = [9, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
     assert bowling_score(frames) == 20
 
 def test_gameWithOneSpareAtLastFrame():
-    frames = [(1, 0) for x in range(10)]
-    frames[-1] = (9, 1, 0)
+    frames = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 9, 1, 0]
     assert bowling_score(frames) == 19
 
 def test_gameWithOneStrikeAtFirstFrame():
-    frames = [(1, 0) for x in range(10)]
-    frames[0] = STRIKE
+    frames = [10, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
     assert bowling_score(frames) == 20
 
 def test_gameWithOneStrikeAtLastFrame():
-    frames = [(1, 0) for x in range(10)]
-    frames[-1] = (10, 0, 0)
+    frames = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 10, 0, 0]
     assert bowling_score(frames) == 19
 
 def test_gameWithOnlySpares():
-    frames = [(9, 1) for x in range(10)]
-    frames[-1] = (9, 1, 1)
+    frames = [9, 1,9, 1,9, 1,9, 1,9, 1,9, 1,9, 1,9, 1,9, 1,9, 1,1]
     assert bowling_score(frames) == 182
 
 def test_gameWith2StrickesInARowAtBegin():
-    frames = [(1, 0) for x in range(10)]
-    frames[0] = frames[1] = (10,0)
+    frames = [10, 10, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
     assert bowling_score(frames) == 40
 
 def test_gameWith2StrickesInARowAtEnd():
-    frames = [(1, 0) for x in range(10)]
-    frames[-2] = STRIKE
-    frames[-1] = (10, 0, 0)
+    frames = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 10, 10, 0, 0]
     assert bowling_score(frames) == 38
 
 def test_gameWithOnlyStrikes():
-    frames = [STRIKE for x in range(10)]
-    frames[-1] = (10, 10, 10)
+    frames = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
     assert bowling_score(frames) == 300
 
-def test_notCompletedGame():
-    frames = [(10, 0), (3, 2), (5, 4), (10, 0)]
-    assert bowling_score(frames) == 29
-    assert best_possible_score(frames) == 239
+# def test_notCompletedGame():
+#     frames = [(10, 0), (3, 2), (5, 4), (10, 0)]
+#     assert bowling_score(frames) == 29
+#     assert best_possible_score(frames) == 239
 
-    frames = [(4, 0), (9, 0), (1, 5), (7, 3), (9, 0), (4, 5), (7, 0), (7, 2), (9, 0)]
-    assert bowling_score(frames) == 81
-    assert best_possible_score(frames) == 111
+#     frames = [(4, 0), (9, 0), (1, 5), (7, 3), (9, 0), (4, 5), (7, 0), (7, 2), (9, 0)]
+#     assert bowling_score(frames) == 81
+#     assert best_possible_score(frames) == 111
 
 def test_fromRandomGame():
-    frames = [(7, 1), (5, 4), (5, 2), (6, 4), (4, 1), (8, 1), (2, 8), (0, 1), (7, 0), (10, 10, 9)]
+    frames = [7, 1, 5, 4, 5, 2, 6, 4, 4, 1, 8, 1, 2, 8, 0, 1, 7, 0, 10, 10, 9]
     assert bowling_score(frames) == 99
 
-    frames = [(7, 1), (1, 9), (8, 2), (4, 5), (10, 0), (4, 0), (9, 0), (4, 4), (1, 7), (2, 8, 10)]
-    assert bowling_score(frames) == 112
